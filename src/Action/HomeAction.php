1a2390bc593ba04671fc391bd04846e834437259
VShafeeva <rolf_73@mail.ru>
2018-12-11 14:41:57 +0400

    Включил SimpleLogger

--- src/Action/HomeAction.php
+++ src/Action/HomeAction.php
@@ -11,7 +11,7 @@ final class HomeAction
     protected $renderer;
     protected $logger;
 
-    public function __construct($view, $logger)
+    public function __construct($view,AdapterInterface $logger)
     {
         $this->renderer = $view;
         $this->logger = $logger;
